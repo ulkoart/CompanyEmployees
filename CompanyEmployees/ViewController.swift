910e4745fc0e9733182106acdc5fd874f824bd91
ulkoart <ulkoart@gmail.com>
2021-07-13 22:48:59 +0300

    Initial Commit

---
+++ CompanyEmployees/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompanyEmployees
+//
+//  Created by user on 13.07.2021.
+//  Copyright © 2021 ulkoart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
